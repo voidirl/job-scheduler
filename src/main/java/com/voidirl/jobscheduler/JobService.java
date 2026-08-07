99f79fe7694b1cff5ceb9b13ad210b678a23f9ae
voidirl <rishavrajput204@gmail.com>
2026-08-07 12:08:33 +0530

    add JobService with business logic

---
+++ src/main/java/com/voidirl/jobscheduler/JobService.java
@@ -0,0 +1,29 @@
+package com.voidirl.jobscheduler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public Job createJob(String jobName, LocalDateTime scheduledTime){
+        Job job = new Job();
+        job.setJobName(jobName);
+        job.setScheduledTime(scheduledTime);
+        job.setStatus(JobStatus.SCHEDULED);
+        job.setCreatedAt(LocalDateTime.now());
+        return jobRepository.save(job);
+    }
+    public List<Job> gelAllJobs(){
+        return jobRepository.findAll();
+    }
+    public List<Job> getJobsByStatus(JobStatus status){
+        return jobRepository.findByStatus(status);
+    }
+}
